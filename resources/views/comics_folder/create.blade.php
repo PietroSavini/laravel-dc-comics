612ab170c54fa0d93e64b1ac2b76182924023168
PietroSavini <savinipietro@gmail.com>
2023-06-05 17:58:20 +0200

    ADD: old values on inputs for better UE

--- resources/views/comics_folder/create.blade.php
+++ resources/views/comics_folder/create.blade.php
@@ -1,79 +1,84 @@
 @extends('Layouts.App')
 
 @section('content')
-<form action="{{route('comics_folder.store')}}" method="POST">
-  @csrf
-  <h2 class="text-center my-3">Nuovo Comic:</h2>
+    <form action="{{ route('comics_folder.store') }}" method="POST">
+        @csrf
+        <h2 class="text-center my-3">Nuovo Comic:</h2>
 
-    @if ($errors->any())
-        <ul class="alert alert-danger">
-          <h2 class="text-center">verifica l'inserimento dei dati:</h2>
-          @foreach ($errors->all() as $error)
-              
-              <li class="m-3">{{$error}}</li>
-          @endforeach
-        </ul>
-    @endif
+        @if ($errors->any())
+            <ul class="alert alert-danger">
+                <h2 class="text-center">verifica l'inserimento dei dati:</h2>
+                @foreach ($errors->all() as $error)
+                    <li class="m-3">{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
 
-    <div class="form-group">
-      <label for="title">titolo</label>
-      <input type="text" class="form-control @error('title')is-invalid @enderror" id="title" name="title">
-      @error('title')
-        <div class="invalid-feedback">
-           {{ $message }}
+        <div class="form-group">
+            <label for="title">titolo</label>
+            <input value="{{ old('title') }}" type="text" class="form-control @error('title')is-invalid @enderror"
+                id="title" name="title">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-      @enderror
-    </div>
-    <div class="form-group">
-      <label for="img">immagine</label>
-      <input type="text" class="form-control @error('thumb')is-invalid @enderror" id="img" name="thumb">
-      @error('thumb')
-        <div class="invalid-feedback">
-           {{ $message }}
+        <div class="form-group">
+            <label for="img">immagine</label>
+            <input value="{{ old('thumb') }}" type="text" class="form-control @error('thumb')is-invalid @enderror"
+                id="img" name="thumb">
+            @error('thumb')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-      @enderror
-    </div>
-    <div class="form-group">
-      <label for="price">prezzo</label>
-      <input type="text" class="form-control @error('price')is-invalid @enderror" id="price" name="price">
-      @error('price')
-        <div class="invalid-feedback">
-           {{ $message }}
+        <div class="form-group">
+            <label for="price">prezzo</label>
+            <input value="{{ old('price') }}" type="text" class="form-control @error('price')is-invalid @enderror"
+                id="price" name="price">
+            @error('price')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-      @enderror
-    </div>
-    <div class="form-group">
-      <label for="series">series</label>
-      <input type="text" class="form-control @error('series')is-invalid @enderror" id="series" name="series">
-      @error('series')
-        <div class="invalid-feedback">
-           {{ $message }}
+        <div class="form-group">
+            <label for="series">series</label>
+            <input value="{{ old('series') }}" type="text" class="form-control @error('series')is-invalid @enderror"
+                id="series" name="series">
+            @error('series')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-      @enderror
-    </div>
-    <div class="form-group">
-      <label for="sale_date">data di uscita</label>
-      <input type="date" class="form-control @error('sale_date')is-invalid @enderror" id="sale_date" name="sale_date">
-      @error('sale_date')
-        <div class="invalid-feedback">
-           {{ $message }}
+        <div class="form-group">
+            <label for="sale_date">data di uscita</label>
+            <input value="{{ old('sale_date') }}" type="date"
+                class="form-control @error('sale_date')is-invalid @enderror" id="sale_date" name="sale_date">
+            @error('sale_date')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-      @enderror
-    </div>
-    <div class="form-group">
-        <label for="tipo">tipo</label>
-        <input type="text" class="form-control @error('type')is-invalid @enderror" id="tipo" name="type">
-        @error('type')
-        <div class="invalid-feedback">
-           {{ $message }}
+        <div class="form-group">
+            <label for="tipo">tipo</label>
+            <input value="{{ old('type') }}" type="text" class="form-control @error('type')is-invalid @enderror"
+                id="tipo" name="type">
+            @error('type')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-      @enderror
-      </div>
-    <div class="form-group">
-      <label for="description" class="d-block">descrizione</label>
-      <textarea name="description" id="description" cols="92" rows="10"></textarea>
-    </div>
-    
-    <button type="submit" class="btn btn-primary">aggiungi comic</button>
-  </form>
+        <div class="form-group">
+            <label for="description" class="d-block">descrizione</label>
+            <textarea name="description" id="description" cols="92" rows="10"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">aggiungi comic</button>
+    </form>
 @endsection
--- resources/views/comics_folder/editelement.blade.php
+++ resources/views/comics_folder/editelement.blade.php
@@ -1,79 +1,74 @@
 @extends('Layouts.App')
 
 @section('content')
-<form action="{{route('comics_folder.update', $comic->id)}}" method="POST">
-  @method('PUT')
-  @csrf
-  <h2 class="text-center my-3">modifica del comic: {{$comic->title}}</h2>
-  @if ($errors->any())
-  <ul class="alert alert-danger">
-    <h2 class="text-center">verifica l'inserimento dei dati:</h2>
-    @foreach ($errors->all() as $error)
-        
-        <li class="m-3">{{$error}}</li>
-    @endforeach
-  </ul>
-@endif
+    <form action="{{ route('comics_folder.update', $comic->id) }}" method="POST">
+        @method('PUT')
+        @csrf
+        <h2 class="text-center my-3">modifica del comic: {{ $comic->title }}</h2>
+        @if ($errors->any())
+            <ul class="alert alert-danger">
+                <h2 class="text-center">verifica l'inserimento dei dati:</h2>
+                @foreach ($errors->all() as $error)
+                    <li class="m-3">{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
 
-<div class="form-group">
-<label for="title">titolo</label>
-<input type="text" class="form-control @error('title')is-invalid @enderror" id="title" name="title">
-@error('title')
-  <div class="invalid-feedback">
-     {{ $message }}
-  </div>
-@enderror
-</div>
-<div class="form-group">
-<label for="img">immagine</label>
-<input type="text" class="form-control @error('thumb')is-invalid @enderror" id="img" name="thumb">
-@error('thumb')
-  <div class="invalid-feedback">
-     {{ $message }}
-  </div>
-@enderror
-</div>
-<div class="form-group">
-<label for="price">prezzo</label>
-<input type="text" class="form-control @error('price')is-invalid @enderror" id="price" name="price">
-@error('price')
-  <div class="invalid-feedback">
-     {{ $message }}
-  </div>
-@enderror
-</div>
-<div class="form-group">
-<label for="series">series</label>
-<input type="text" class="form-control @error('series')is-invalid @enderror" id="series" name="series">
-@error('series')
-  <div class="invalid-feedback">
-     {{ $message }}
-  </div>
-@enderror
-</div>
-<div class="form-group">
-<label for="sale_date">data di uscita</label>
-<input type="date" class="form-control @error('sale_date')is-invalid @enderror" id="sale_date" name="sale_date">
-@error('sale_date')
-  <div class="invalid-feedback">
-     {{ $message }}
-  </div>
-@enderror
-</div>
-<div class="form-group">
-  <label for="tipo">tipo</label>
-  <input type="text" class="form-control @error('type')is-invalid @enderror" id="tipo" name="type">
-  @error('type')
-  <div class="invalid-feedback">
-     {{ $message }}
-  </div>
-@enderror
-</div>
-<div class="form-group">
-<label for="description" class="d-block">descrizione</label>
-<textarea name="description" id="description" cols="92" rows="10"></textarea>
-</div>
+        <div class="form-group">
+            <label for="img">immagine</label>
+            <input value="{{ old('thumb') }}" type="text" class="form-control @error('thumb')is-invalid @enderror"
+                id="img" name="thumb">
+            @error('thumb')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="price">prezzo</label>
+            <input value="{{ old('price') }}" type="text" class="form-control @error('price')is-invalid @enderror"
+                id="price" name="price">
+            @error('price')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="series">series</label>
+            <input value="{{ old('series') }}" type="text" class="form-control @error('series')is-invalid @enderror"
+                id="series" name="series">
+            @error('series')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="sale_date">data di uscita</label>
+            <input value="{{ old('sale_date') }}" type="date"
+                class="form-control @error('sale_date')is-invalid @enderror" id="sale_date" name="sale_date">
+            @error('sale_date')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="tipo">tipo</label>
+            <input value="{{ old('type') }}" type="text" class="form-control @error('type')is-invalid @enderror"
+                id="tipo" name="type">
+            @error('type')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="description" class="d-block">descrizione</label>
+            <textarea name="description" id="description" cols="92" rows="10"></textarea>
+        </div>
 
-<button type="submit" class="btn btn-primary">Applica modifiche</button>
-  </form>
+        <button type="submit" class="btn btn-primary">Applica modifiche</button>
+    </form>
 @endsection
